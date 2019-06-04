5b2f78e859a2086239d6ba49dda0b996d5380c80
“lukemccrae” <lukemccrae@gmail.com>
2019-06-04 12:18:19 -0700

    remove unused lines

--- server/app.js
+++ server/app.js
@@ -16,9 +16,6 @@ var users = require('./routes/users');
 
 const MONGODB_URI = process.env.MONGOLAB_URI || process.env.LOCAL_HOST;
 
-// var mongodb = process.env.API_HOST;
-console.log(MONGODB_URI, 'mongodb log');
-
 const options = {
     autoIndex: false, // Don't build indexes
     reconnectTries: 30, // Retry up to 30 times
